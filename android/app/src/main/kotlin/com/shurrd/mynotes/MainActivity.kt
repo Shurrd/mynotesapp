5ea357bce3dabcd4dadfba2dd89e47d40cb89137
Shurrd <engraaadeyemi@gmail.com>
2023-09-28 10:11:25 +0100

    first commit

---
+++ android/app/src/main/kotlin/com/shurrd/mynotes/MainActivity.kt
@@ -0,0 +1,6 @@
+package com.shurrd.mynotes
+
+import io.flutter.embedding.android.FlutterActivity
+
+class MainActivity: FlutterActivity() {
+}
---
+++ lib/main.dart
@@ -0,0 +1,55 @@
+import 'package:flutter/material.dart';
+
+void main() {
+  runApp(MaterialApp(
+    title: 'Flutter Demo',
+    theme: ThemeData(
+      colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+      useMaterial3: true,
+    ),
+    home: const HomePage(),
+  ));
+}
+
+class HomePage extends StatefulWidget {
+  const HomePage({super.key});
+
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  late final TextEditingController _email;
+  late final TextEditingController _password;
+
+  @override
+  void initState() {
+    _email = TextEditingController(); // TODO: implement initState
+    _password = TextEditingController(); // TODO: implement initState
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    _email.dispose(); // TODO: implement dispose
+    _password.dispose(); // TODO: implement dispose
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(title: const Text("Register")),
+      body: Column(
+        children: [
+          TextField(),
+          TextField(),
+          TextButton(
+            onPressed: () async {},
+            child: const Text("Register"),
+          ),
+        ],
+      ),
+    );
+  }
+}

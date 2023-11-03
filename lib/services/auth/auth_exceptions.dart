5f506ff4fd396ea3eebd099b9b44479efe3b6319
Shurrd <engraaadeyemi@gmail.com>
2023-11-03 15:27:58 +0100

    firebase sercice provider auth

---
+++ lib/services/auth/auth_exceptions.dart
@@ -0,0 +1,17 @@
+// login exceptions
+
+class InvalidUserCredentialsAuthException implements Exception {}
+
+// register exceptions
+
+class WeakPasswordAuthExecption implements Exception {}
+
+class EmailAlreadyInUseAuthExecption implements Exception {}
+
+//  generic execptions
+
+class GenericAuthException implements Exception {}
+
+class UserNotLoggedInAuthException implements Exception {}
+
+class InvalidEmailAuthException implements Exception {}
---
+++ lib/services/auth/firebase_auth_provider.dart
@@ -0,0 +1,3 @@
+import 'package:mynotes/services/auth/auth_user.dart';
+import 'package:mynotes/services/auth/auth_provider.dart';
+import 'package:mynotes/services/auth/auth_exceptions.dart';
